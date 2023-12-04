f5834eb60e8c176015267a30a51297ba9b9f7893
soborg <167809+soborg@users.noreply.github.com>
2023-12-04 09:28:39 +0100

    Update gm-travexport.user.js
    Massive cleanup of resulting files. Overall more readable for humans and machines alike.

--- gm-travexport.user.js
+++ gm-travexport.user.js
@@ -3,12 +3,20 @@
 // @namespace    https://github.com/soborg/travExport
 // @description  Download notes and stuff from a Traverse deck
 // @author			 soborg
-// @version      0.4
+// @version      0.5
 // @grant        none
 // @match        https://traverse.link/*
 // ==/UserScript==
 
 // Version history
+//
+// 0.5   (2023-12-04): a whole lot of cleanup for the final cards.
+//                     Review/visit/and other Traverse specific fields are removed
+//                     Usable fields are moved to a higher level in the json fields.
+//                     Some fields are parsed to be more readable (for both human and machine):
+//                     - lists of audio file links, list of tags, etc.
+//                     - props/actors/etc. are stored by key instead of text-blobs
+//                     - highlighted words in sentences are extracted and stored separately
 //
 //
 // 0.4   (2023-11-12): nullify some more fields from the exported cards.
@@ -66,14 +74,84 @@
           for (jdx in Object.keys(unsafeWindow.elm)) {
             var key = Object.keys(unsafeWindow.elm)[jdx];
             if (key.startsWith('__reactFiber')) {
-              var card = {...unsafeWindow.elm[key].child.pendingProps.children.props.data.card};
-              card.user = null;
-              card.graphInfo = null;
-              card.topicCard = null;
+              var card = {...unsafeWindow.elm[key].child.pendingProps.children.props.data.card}; // perform a shallow copy of the object
+              // remove a lot of irrelevant fields
+              delete card.user;
+              delete card.graphInfo;
+              delete card.cardUserLoaded;
+              delete card.isDummy;
+              delete card.topicCard;
+              delete card.templateRebuildTime;
+              delete card.allowDuplicate;
+              delete card.free;
+              
               for (var u in card.users) {
-                card.users[u].fieldsHtml = null;
-                card.users[u].users = null;
+                delete card.users[u].fieldsHtml;
+                delete card.users[u].users;
+                delete card.users[u].notes;
+                delete card.users[u].prompts;
+                delete card.users[u].reviews;
+                delete card.users[u].finished;
+                delete card.users[u].seen;
+                delete card.users[u].lastOpened;
+                delete card.users[u].dueTime;
+                delete card.users[u].reviewTime;
+                if (u === 'Mandarin_Blueprint') {
+                	card.fields = {...card.users[u].fields};
+                  // a whole lot of cleanup
+                  if (card.fields.Sentence) {
+                    card.fields['Highlights'] = [];
+                    var sentsplit = card.fields.Sentence.split('==');
+                    for (var spl in sentsplit) {
+                      if (spl % 2 == 1) {
+                        card.fields['Highlights'].push(sentsplit[spl]);
+                      }
+                    }
+                    // var highlighted_word = card.fields.Sentence.split('==')[1];
+                    // card.fields['Highlight'] = highlighted_word;
+                  }
+                  if (card.fields["Top-Down Word(s)"]) {
+                    card.fields["Top-Down Word(s)"] = card.fields["Top-Down Word(s)"].replaceAll('&nbsp;', '').split('\n')
+                  }
+                  if (card.fields.Tags) {
+                    card.fields.Tags = card.fields.Tags.split('\n');
+                  }
+                  if (card.fields.Audio) {
+                  	card.fields.Audio = card.fields.Audio.replaceAll('&nbsp;', '').split('\n').filter((x) => x.replaceAll(' ', '').length > 5);
+                  }
+                  if (card.fields.AUDIO) {
+                  	card.fields.AUDIO = card.fields.AUDIO.replaceAll('&nbsp;', '').split('\n').filter((x) => x.replaceAll(' ', '').length > 5);
+                  }
+                  if (card.fields.Links) {
+                    var link_map = {};
+                    var splitvalues = card.fields.Links.replaceAll('&nbsp;', '').split('**')
+                    var prev_key = null;
+
+                    for (splitidx in splitvalues) {
+                      var splitvalue = splitvalues[splitidx].replaceAll(' ', '').replaceAll('\n', '').replaceAll(':', '');
+                      if (splitvalue.length == 0) {
+                        continue;
+                      }
+                      else if (prev_key != null && prev_key.length > 0) {
+                        if (prev_key == 'Prop(s)') {
+													splitvalue = splitvalue.split(',');
+                        }
+                        link_map[prev_key] = splitvalue;
+                        prev_key = null;
+                        continue
+                      } else {
+                        prev_key = splitvalue.replaceAll(' ', '');
+                      }
+                    }
+
+										card.fields.Links = link_map;
+                    
+                  }
+                } else {
+                  card.user_fields = {...card.users[u].fields};
+                }
               }
+              delete card.users;
               cards.push(card);
               break;
             }
@@ -90,6 +168,23 @@
     }
     var filename = document.getElementsByClassName('text-lg text-black')[0].textContent.replaceAll(' ', '');
    	download(filename + '.json', cards);
+  };
+
+  function expand(deck) {
+    try {
+    	deck.click();
+    }
+    catch {
+      console.log('obj has no click function: ', deck);
+  	}
+  };
+
+  function expandTopicDecks() {
+    var decks = document.getElementsByClassName("react-flow__node react-flow__node-groupNode selectable parent");
+    for (var i in decks) {
+      var deck = decks[i];
+    	window.setTimeout(expand, 1000*i, deck);
+    }
   };
 
   function createDownloadButton() {
@@ -120,6 +215,6 @@
     }
   }
 
-  window.setInterval(areWeThereYet, 5000); // occasional check to see if we're (still) in the right spot
+  window.setInterval(areWeThereYet, 5000); // occasional check to see if we're in the right spot
 
 })();
