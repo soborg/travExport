2f4cc9663b6dd825470266078d130ee350347c71
soborg <167809+soborg@users.noreply.github.com>
2023-12-05 06:57:46 +0100

    Update gm-travexport.user.js
    Add dropdown menu and some code structure improvements.
    
    Add button to expand all decks (fixes #9)

--- gm-travexport.user.js
+++ gm-travexport.user.js
@@ -3,12 +3,21 @@
 // @namespace    https://github.com/soborg/travExport
 // @description  Export your notes from a Traverse (Mandarin Blueprint) deck to JSON
 // @author			 soborg
-// @version      0.9
-// @grant        unsafeWindow
+// @version      0.10
+// @grant        unsafeWindow,GM_addStyle
 // @match        https://traverse.link/*
 // ==/UserScript==
 
 // Version history
+//
+//
+// 0.10  (2023-12-05): button replaced by a nice fancy menu, adding:
+//                     - button to export as JSON
+//                     - button to expand all decks (with an ugly progress counter)
+//                     - link to the project github page
+//
+//                     restructure collectAndDownload function, to break/continue earlier.
+//                     no longer throw away errors that was caused by card parsing errors.
 //
 //
 // 0.9   (2023-12-04): cleaned up most (all?) of the markup tags, showing only their relevant links or values.
@@ -55,8 +64,28 @@
 
 
 (function() {
+    
+  function GM_addStyle(css) {
+     const style = document.getElementById("GM_addStyleByTravExport") || (function() {
+      const style = document.createElement('style');
+      style.type = 'text/css';
+      style.id = "GM_addStyleByTravExport";
+      document.head.appendChild(style);
+      return style;
+    })();
+    const sheet = style.sheet;
+    sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
+  };
 
-  download = function(filename, text) {
+  GM_addStyle(".dropbtn { color: black; padding: 16px; font-size: 16px; border: none; cursor: pointer; }");
+  GM_addStyle(".dropbtn:hover { background-color: #2980B9; }");
+  GM_addStyle(".dropdown { float: right; position: relative; display: inline-block; }");
+  GM_addStyle(".dropdown-content { display: none; position: absolute; background-color: #f1f1f1; min-width: 160px; overflow: auto; box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2); right: 0; z-index: 1; }");
+  GM_addStyle(".dropdown-content a { color: black; padding: 12px 16px; text-decoration: none; display: block; cursor: pointer; }");
+  GM_addStyle(".dropdown a:hover {background-color: #ddd; }");
+  GM_addStyle(".show {display: block;}");
+  
+  function download(filename, text) {
     var element = document.createElement('a');
     element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(text)));
     element.setAttribute('download', filename);
@@ -67,6 +96,8 @@
   };
 
   unMarkupifyLink = function(x) {
+    if (x.length == 0)
+      return x;
     var link = x.split("]")[0].replace("[", "").replace("!", "");
     if (link.length == 0) {
       link = x.split("(")[1].replace(")", "");
@@ -110,146 +141,201 @@
     for (idx in topics) {
       // filter out parents
       var elm = topics[idx];
+      var isParent = null;
       try {
-        if (elm.getAttribute('class').indexOf('parent') > 0) {
-          console.debug('object is a parent of sort, skipping');
-        } else {
-          unsafeWindow.elm = elm;  // storing the element on the unsafeWindow exposes the internal React properties so we can access the nested card data
-          for (jdx in Object.keys(unsafeWindow.elm)) {
-            var key = Object.keys(unsafeWindow.elm)[jdx];
-            if (key.startsWith('__reactFiber')) {
-              var card = {...unsafeWindow.elm[key].child.pendingProps.children.props.data.card}; // perform a shallow copy of the object
-              // remove a lot of irrelevant fields
-              delete card.user;
-              delete card.graphInfo;
-              delete card.cardUserLoaded;
-              delete card.isDummy;
-              card.deckTitle = card.topicCard.title; // save the title for toplevel
-              delete card.topicCard;             // then kill it, as it is recursive
-              delete card.templateRebuildTime;
-              delete card.allowDuplicate;
-              delete card.free;
-              delete card.flowNode;
-              delete card.flowChildren;
-              delete card.flowParents;
-              delete card.doRepeat;
-              delete card.appearance;
+        isParent = elm.getAttribute('class').indexOf('parent') > 0
+      } catch {
+     		continue    
+      }
+      if (isParent) {
+        console.debug('object is a parent of sort, skipping');
+        continue
+      }
+      
+      unsafeWindow.elm = elm;  // storing the element on the unsafeWindow exposes the internal React properties so we can access the nested card data
+      for (jdx in Object.keys(unsafeWindow.elm)) {
+        var key = Object.keys(unsafeWindow.elm)[jdx];
+        if (!key.startsWith('__reactFiber')) {
+          console.warning('reactFiber could not be found in element: ', elm);
+          continue
+        }
+        var card = {...unsafeWindow.elm[key].child.pendingProps.children.props.data.card}; // perform a shallow copy of the object
+        // remove a lot of irrelevant fields
+        delete card.user;
+        delete card.graphInfo;
+        delete card.cardUserLoaded;
+        delete card.isDummy;
+        card.deckTitle = card.topicCard.title; // save the title for toplevel
+        delete card.topicCard;             // then kill it, as it is recursive
+        delete card.templateRebuildTime;
+        delete card.allowDuplicate;
+        delete card.free;
+        delete card.flowNode;
+        delete card.flowChildren;
+        delete card.flowParents;
+        delete card.doRepeat;
+        delete card.appearance;
 
-              for (var u in card.users) {
-                if (u === 'Mandarin_Blueprint') {
-                	card.fields = {...card.users[u].fields};
-                  // a whole lot of cleanup
-                  if (card.fields.Sentence) {
-                    card.fields['Highlights'] = [];
-                    var sentsplit = card.fields.Sentence.split('==');
-                    for (var spl in sentsplit) {
-                      if (spl % 2 == 1) {
-                        var w = sentsplit[spl];
-                        if (card.fields['Highlights'].indexOf(w) < 0) {
-                       	  card.fields['Highlights'].push(sentsplit[spl]);
-                        }
-                      }
-                    }
-                  }
-                  if (card.fields["Top-Down Word(s)"]) {
-                    card.fields["Top-Down Word(s)"] = card.fields["Top-Down Word(s)"].replaceAll('&nbsp;', '').split('\n')
-                  }
-                  if (card.fields.Tags) {
-                    card.fields.Tags = card.fields.Tags.split('\n').map(unMarkupifyLink);
-                  }
-                  if (card.fields.Audio) {
-                  	card.fields.Audio = card.fields.Audio.replaceAll('&nbsp;', '').split('\n').filter((x) => x.replaceAll(' ', '').length > 5).map(unMarkupifyLink);
-                  }
-                  if (card.fields.AUDIO) {
-                  	card.fields.AUDIO = card.fields.AUDIO.replaceAll('&nbsp;', '').split('\n').filter((x) => x.replaceAll(' ', '').length > 5).map(unMarkupifyLink);
-                  }
-                  if (card.fields["STROKE ORDER"]) {
-                    card.fields["STROKE ORDER"] = unMarkupifyLink(card.fields["STROKE ORDER"]);
-                  }
-
-                  if (card.fields.Links) {
-                    var link_map = {};
-                    var splitvalues = card.fields.Links.replaceAll('&nbsp;', '').split('**')
-                    var prev_key = null;
-
-                    for (splitidx in splitvalues) {
-                      var splitvalue = splitvalues[splitidx].replaceAll(' ', '').replaceAll('\n', '').trim();
-                      if (splitvalue.length == 0) {
-                        continue;
-                      }
-                      else if (prev_key != null && prev_key.length > 0) {
-                        if (prev_key == "Characters") {
-                          splitvalue = splitvalue.split(')[').map(unMarkupifyLink).map(decodeComp);
-                        } else if (prev_key == "Prop(s)") {
-                          splitvalue = splitvalue.split(',').map(unMarkupifyLink).map(decodeComp);
-                        } else {
-                          splitvalue = decodeComp(unMarkupifyLink(splitvalue));
-                        }
-                        link_map[prev_key] = splitvalue
-                        prev_key = null;
-                        continue
-                      } else {
-                        prev_key = splitvalue.replaceAll(' ', '').replaceAll(":", "");
-                      }
-                    }
-                    card.fields.Links = link_map;
-                  }
-                } else {
-                  card.user_fields = {...card.users[u].fields};
-                  if (card.user_fields["NOTES"]) {
-                    card.user_fields["NOTES"] = cleanUserField(card.user_fields["NOTES"]);
-                  }
+        for (var u in card.users) {
+          if (u !== "Mandarin_Blueprint") {
+            card.user_fields = {...card.users[u].fields};
+            if (card.user_fields["NOTES"]) {
+              card.user_fields["NOTES"] = cleanUserField(card.user_fields["NOTES"]);
+            }
+            continue
+          }
+          card.fields = {...card.users[u].fields};
+          // a whole lot of cleanup
+          if (card.fields.Sentence) {
+            card.fields['Highlights'] = [];
+            var sentsplit = card.fields.Sentence.split('==');
+            for (var spl in sentsplit) {
+              if (spl % 2 == 1) {
+                var w = sentsplit[spl];
+                if (card.fields['Highlights'].indexOf(w) < 0) {
+                  card.fields['Highlights'].push(sentsplit[spl]);
                 }
               }
-              delete card.users;
-              cards.push(card);
-              break;
-            }
-            else {
-              console.warning('reactFiber could not be found in element: ', elm);
             }
           }
+          if (card.fields["Top-Down Word(s)"]) {
+            card.fields["Top-Down Word(s)"] = card.fields["Top-Down Word(s)"].replaceAll('&nbsp;', '').split('\n')
+          }
+          if (card.fields.Tags) {
+            card.fields.Tags = card.fields.Tags.split('\n').map(unMarkupifyLink);
+          }
+          if (card.fields.Audio) {
+            card.fields.Audio = card.fields.Audio.replaceAll('&nbsp;', '').split('\n').filter((x) => x.replaceAll(' ', '').length > 5).map(unMarkupifyLink);
+          }
+          if (card.fields.AUDIO) {
+            card.fields.AUDIO = card.fields.AUDIO.replaceAll('&nbsp;', '').split('\n').filter((x) => x.replaceAll(' ', '').length > 5).map(unMarkupifyLink);
+          }
+          if (card.fields["STROKE ORDER"]) {
+            card.fields["STROKE ORDER"] = unMarkupifyLink(card.fields["STROKE ORDER"]);
+          }
+
+          if (card.fields.Links) {
+            var link_map = {};
+            var splitvalues = card.fields.Links.replaceAll('&nbsp;', '').split('**')
+            var prev_key = null;
+
+            for (splitidx in splitvalues) {
+              var splitvalue = splitvalues[splitidx].replaceAll(' ', '').replaceAll('\n', '').trim();
+              if (splitvalue.length == 0) {
+                continue;
+              }
+              if (prev_key != null && prev_key.length > 0) {
+                if (prev_key == "Characters") {
+                  splitvalue = splitvalue.split(')').map(unMarkupifyLink).map(decodeComp).filter((x) => x.length > 0);
+                } else if (prev_key == "Prop(s)") {
+                  splitvalue = splitvalue.split(',').map(unMarkupifyLink).map(decodeComp).filter((x) => x.length > 0);
+                } else {
+                  splitvalue = decodeComp(unMarkupifyLink(splitvalue));
+                }
+                link_map[prev_key] = splitvalue
+                prev_key = null;
+                continue
+              }
+              prev_key = splitvalue.replaceAll(' ', '').replaceAll(":", "");
+
+            }
+            card.fields.Links = link_map;
+          }
         }
+        delete card.users;
+        cards.push(card);
+        break;
       }
-      catch {
-        // apparently, some of the elements are functions and stuff, that does not have a 'getAttribute' function... soooooooooo, ignore (TODO: make better instead of try-catch)
-        ;
-   		}
     }
     var filename = document.getElementsByClassName('text-lg text-black')[0].textContent.replaceAll(' ', '');
    	download(filename + '.json', cards);
   };
 
-  function expand(deck) {
+  var exported = 0;
+  function dropdownCount(num) {
+      var dropdown = document.getElementsByClassName("dropbtn")[0];
+      dropdown.textContent = `E (${exported}/${num})`;
+      if (exported == num) { // reset state
+        dropdown.textContent = `E`;
+        exported = 0;
+      }
+  }
+  
+  function expand(deck, decknum) {
     try {
-    	deck.click();
+      dropdownCount(decknum);
+      exported += 1;
+      deck.click();
     }
     catch {
       console.log('obj has no click function: ', deck);
     }
   };
 
-  function expandTopicDecks() {
+  expandTopicDecks = function() {
     var decks = document.getElementsByClassName("react-flow__node react-flow__node-groupNode selectable parent");
+  	var dropdown = document.getElementsByClassName("dropbtn")[0];
+  	var decknum = decks.length;
+  	dropdownCount(decknum);
     for (var i in decks) {
       var deck = decks[i];
-      window.setTimeout(expand, 1000*i, deck);
+      window.setTimeout(expand, 1000*i, deck, decknum);
     }
   };
 
+  function myDropdown() {
+    var dropdown = document.getElementById("myDropdown");
+    dropdown.classList.toggle('show');
+  }
+
   function createDownloadButton() {
+    document.addEventListener('click', (event) => {
+      if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    });
+    var outer_div = document.createElement('div');
+    outer_div.classList.toggle("dropdown");
     var button = document.createElement('button');
-    button.setAttribute('class', 'homescreen-button');
-    button.addEventListener("click", collectAndDownloadCards, false);
-    var span = document.createElement('span');
-    span.textContent = "E";
-    span.setAttribute("title", "Export the current level to JSON");
-    span.setAttribute("class", "text-gray-800");
-    console.debug("appending span");
-    button.appendChild(span);
+    button.textContent = 'E';
+    button.classList.toggle('homescreen-button');
+    button.classList.toggle('dropbtn');
+    button.addEventListener('click', myDropdown, false);
+    outer_div.appendChild(button);
+    var inner_div = document.createElement('div');
+    inner_div.id = 'myDropdown';
+    inner_div.classList.toggle("dropdown-content");
+    outer_div.appendChild(inner_div);
+    
+    var a_export = document.createElement('a');
+ //   a_export.setAttribute('href', '#');
+    a_export.textContent = 'Export as JSON';
+    a_export.addEventListener('click', collectAndDownloadCards, false);
+    inner_div.appendChild(a_export);
+
+    var a_expand = document.createElement('a');
+ //   a_expand.setAttribute('href', '#');
+    a_expand.textContent = 'Expand all decks (takes a few seconds)';
+    a_expand.addEventListener('click', expandTopicDecks, false);
+    inner_div.appendChild(a_expand);
+
+    var a_about = document.createElement('a');
+    a_about.setAttribute('href', 'https://github.com/soborg/travExport');
+    a_about.setAttribute('target', '_blank');
+    a_about.textContent = 'What is this?';
+    a_about.setAttribute('title', 'Open the travExport Github page');
+    inner_div.appendChild(a_about);
+
     var toolbar = document.getElementsByClassName('MuiToolbar-regular')[0];
     var anchor = toolbar.getElementsByClassName('homescreen-button')[0].parentNode;
-    anchor.appendChild(button);
+    anchor.appendChild(outer_div);
     console.debug('download button created');
   };
 
